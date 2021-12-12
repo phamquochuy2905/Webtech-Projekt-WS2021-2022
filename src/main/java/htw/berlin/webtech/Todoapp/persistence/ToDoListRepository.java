f28d97c39d79d06c9ec48ea261ff4fbaf6ab35dd
phamquochuy2905 <phamquochuy2905@gmail.com>
2021-12-12 21:40:52 +0100

    ToDoListRepository added

---
+++ src/main/java/htw/berlin/webtech/Todoapp/persistence/ToDoListRepository.java
@@ -0,0 +1,12 @@
+package htw.berlin.webtech.Todoapp.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ToDoListRepository extends JpaRepository<ToDoListEntity, Long> {
+
+
+}
